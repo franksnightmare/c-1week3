0c60e0fe0dae41f0b0625ef24f2f915086ff467a
franksnightmare <chnstone13backup@gmail.com>
2016-09-28 15:16:41 +0200

    Add files via upload

---
+++ a24/main.cc
@@ -0,0 +1,8 @@
+#include <cpu.h>
+
+int main()
+{
+	Memory memory;
+	CPU cpu(memory);
+	cpu.start();
+}
---
+++ a24/start.cc
@@ -0,0 +1,49 @@
+#include <cpu.h>
+
+CPU::start()
+{
+	while (true)
+	{
+		int opCode = Tokenizer.opcode();
+		switch (opCode)
+		{
+			case (ERR):
+				error();
+				Tokenizer.reset();
+				break;
+			case (MOV):
+				mov();
+				Tokenizer.reset();
+				break;
+			case (ADD):
+				add();
+				Tokenizer.reset();
+				break;
+			case (SUB):
+				sub();
+				Tokenizer.reset();
+				break;
+			case (MUL):
+				mul();
+				Tokenizer.reset();
+				break;
+			case (DIV):
+				div();
+				Tokenizer.reset();
+				break;
+			case (NEG):
+				neg();
+				Tokenizer.reset();
+				break;
+			case (DSP):
+				dsp();
+				Tokenizer.reset();
+				break;
+			case (STOP)
+				stop();
+				Tokenizer.reset();
+				break;			
+		}
+		Tokenizer.reset();
+	}
+}
